d47677b6f3b2a5434bae2bdbf7830cd356008c7c
testBotXL <aafahim98@gmail.com>
2024-04-03 06:43:58 +0600

    offers1

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
 
 export const Shop = () => {
   return (
     <div>
-        
+        <Hero/>
+        <Popular/>
+        <Offers/>
     </div>
   )
 }
