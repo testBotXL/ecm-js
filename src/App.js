8ca34c25f244339ed7d51b8da805da0e54af16a1
testBotXL <aafahim98@gmail.com>
2024-04-13 16:07:45 +0600

    login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Footer from './Components/Footer/Footer';
 import Shop from './Pages/Shop';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -18,9 +21,9 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner= {men_banner}category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner= {women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner= {kid_banner} category="kid"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
