8b65bf289aec618cdff3b315a3293ad0c6ee0d53
testBotXL <aafahim98@gmail.com>
2024-10-02 15:56:33 +0600

    bug fix update

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import './CSS/LoginSignup.css'
 
-export const LoginSignup = () => {
-  return (
+const LoginSignup = () => {
+  return(
     <div className='loginsignup'>
-      <div className="loginsignup-container">
+      <div className="loginsignup-conteiner">
         <h1>Sign Up</h1>
         <div className="loginsignup-fields">
           <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
+          <input type="email" placeholder='Email Adress' />
           <input type="password" placeholder='Password' />
         </div>
         <button>Continue</button>
-        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
         <div className="loginsignup-agree">
-          <input type='checkbox' name='' id='' />
-          <p>By continuing, You are agree to our terms of use & privacy policy.</p>
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
         </div>
       </div>
-        
+    
     </div>
   )
 }
